31979256f66db637439f632e1ce8c79413af8a3d
Geovane Souza <geovane.saraujo@senacsp.edu.br>
2020-09-15 13:39:28 -0300

    Aplicando strategy em metodos de ordenação

--- src/strategyMetodosOrdenacao/MainStrategy.java
+++ src/strategyMetodosOrdenacao/MainStrategy.java
@@ -12,22 +12,22 @@ public class MainStrategy {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Strategy ordenar = new Strategy();
+        Strategy strategy = new Strategy();
 
         System.out.print("Blubble: ");
         Bubble bubble = new Bubble();
         int bubbleV[] = {5, 3, 2, 1};
-        System.out.println(Arrays.toString(ordenar.ordenar(bubbleV, bubble)));
+        System.out.println(Arrays.toString(strategy.ordenar(bubbleV, bubble)));
 
         System.out.print("Insertion: ");
         Insertion insertion = new Insertion();
-        int InsertionV[] = {5, 3, 2, 1};
-        System.out.println(Arrays.toString(ordenar.ordenar(InsertionV, insertion)));
+        int insertionV[] = {5, 3, 2, 1};
+        System.out.println(Arrays.toString(strategy.ordenar(insertionV, insertion)));
 
         System.out.print("Selection: ");
         Selection selection = new Selection();
         int selectionV[] = {7,5, 3, 2, 1};
-        System.out.println(Arrays.toString(ordenar.ordenar(selectionV, selection)));
+        System.out.println(Arrays.toString(strategy.ordenar(selectionV, selection)));
     }
 
 }
